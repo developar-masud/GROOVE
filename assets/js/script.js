529d320d961cc4a347b22b6431aaa4ca88085873
Masud rana <128996803+developar-masud@users.noreply.github.com>
2024-01-16 16:07:06 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.bar').click(function(){
+        $('.menu ul').toggle(1000);
+    });
+
+    $('.click_heading').click(function(){
+        $('.click_text').toggle();
+    });
+
+});
